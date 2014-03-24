c5d8de8a5a891ad716ef113332cd5c5cd1bf446b
aphramzo <im.t.wal@gmail.com>
2014-03-23 18:38:45 -0600

    Loading up Initial transfers

--- MvcApplication1/Controllers/HomeController.cs
+++ MvcApplication1/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,9 +12,46 @@ namespace MvcApplication1.Controllers
     {
         public ActionResult Index()
         {
+            InsertAutoTransfers();
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
 
             return View();
+        }
+
+        private void InsertAutoTransfers()
+        {
+            SqlConnection sqlConnection1 = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["SQLSERVER_CONNECTION_STRING"].ConnectionString);
+            SqlCommand cmd = new SqlCommand();
+            SqlDataReader reader;
+
+            cmd.CommandText = "select max(transactiondate) from transactions where memo = 'autotransfer'";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = sqlConnection1;
+
+            sqlConnection1.Open();
+
+            reader = cmd.ExecuteReader();
+            // Data is accessible through the DataReader object here.
+            var lastDate = DateTime.Now;
+            while (reader.Read())
+            {
+                lastDate = ReadSingleRow(reader);
+            }
+            reader.Close();
+            for (var i = 1; i <= (DateTime.Now - lastDate).TotalDays;i++)
+            {
+                cmd.CommandText = String.Format("[sp_DailyTransfer] '{0}'", lastDate.AddDays(i));
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = sqlConnection1;
+
+                cmd.ExecuteNonQuery();
+            }
+            sqlConnection1.Close();
+        }
+
+        private DateTime ReadSingleRow(IDataRecord reader)
+        {
+            return (DateTime) reader[0];
         }
 
         public ActionResult About()
---
+++ MvcApplication1/Models/Transaction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class Transaction : BaseModel
+    {
+        public Category Category { get; set; }
+        public DateTime TransactionDate { get; set;}
+        public decimal Amount { get; set; }
+        public string Location { get; set; }
+        public string Memo { get; set; }
+    }
+}
