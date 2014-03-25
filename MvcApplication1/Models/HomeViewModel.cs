67b481f2c0d029fd833ee0b395087c41d4971f66
aphramzo <im.t.wal@gmail.com>
2014-03-24 20:36:34 -0600

    Fix progress bar length

--- MvcApplication1/Models/HomeViewModel.cs
+++ MvcApplication1/Models/HomeViewModel.cs
@@ -10,5 +10,20 @@ namespace MvcApplication1.Models
         public string GroceryMoney { get; set; }
         public string EatingOutMoney { get; set; }
         public string SpendingMoneys { get; set; }
+
+        public int GroceryPercent
+        {
+            get { return Convert.ToInt16(Convert.ToDecimal(GroceryMoney) / 87 * 100); }
+        }
+
+        public int EatingOutPercent
+        {
+            get { return Convert.ToInt16(Convert.ToDecimal(EatingOutMoney) / 81*100); }
+        }
+
+        public int SpendingPercent
+        {
+            get { return Convert.ToInt16(Convert.ToDecimal(SpendingMoneys) / 161*100); }
+        }
     }
 }
